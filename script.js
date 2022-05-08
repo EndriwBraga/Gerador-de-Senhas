60cf6b9620aa19ded3469ff40382049602091f4f
Endriw <endriwbragalisboa@gmail.com>
2022-05-08 19:33:34 -0300

    Commit Comentario linha 9

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password")
 
+//let charset = "abc... são as variaveis que irei selecionar aleatoriamente na função generatePassword()"
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
 let novaSenha = "";
 
